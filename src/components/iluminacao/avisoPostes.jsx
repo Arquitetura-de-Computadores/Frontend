a8e63aa9bfa039025f0b378e7e74a5a1bbbfbce4
Ellen <mariaellensantana725@gmail.com>
2025-03-20 10:14:14 -0300

    fix: :wrench: correção aviso postes

--- src/components/iluminacao/avisoPostes.jsx
+++ src/components/iluminacao/avisoPostes.jsx
@@ -1,10 +1,12 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getLuz } from "@/services/apiRotas";
 
 const AvisoPostes = () => {
+    const pathname = usePathname();
     const [luz, setStatus] = useState(null);
 
     useEffect(() => {
@@ -21,18 +23,20 @@ const AvisoPostes = () => {
     }, []);
     
     return (
-        <div className="mt-10 flex px-20 items-center gap-4 flex-col">
+        <div className="mt-6 flex px-20 items-center gap-4 flex-col">
             <div className="flex px-2 items-center gap-9">
                 <Image src={luz === 1 ? "/Lampada.png" : "/LampadaOff.png"} alt="" width={106} height={106}></Image>
                 <h1 className="font-semibold text-lg text-black">
                     {luz === 1 ? "No momento, as luzes dos postes estão acesas para iluminar as ruas" : "No momento, as luzes dos postes estão apagadas"}
                 </h1>
             </div>
-            <div className="flex justify-end w-full">
-                <Link href="/iluminacao">
-                    <span className="text-[10px] text-gray-500 underline">Como esse sistema funciona?</span>
-                </Link>
-            </div>
+            {pathname !== "/iluminacao" && (
+                <div className="flex justify-end w-full">
+                    <Link href="/iluminacao">
+                        <span className="text-[10px] text-gray-500 underline">Como esse sistema funciona?</span>
+                    </Link>
+                </div>
+            )}
         </div>
     )
 }
